efe4566bc10855158ebb15183112f5f18ec4fd06
Eric Pedrido <epedrids@gmail.com>
2020-03-02 22:28:21 +1300

    Switched array size slider to spinner

--- src/main/java/com/pedrido/javafx/Controller.java
+++ src/main/java/com/pedrido/javafx/Controller.java
@@ -6,34 +6,74 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML public Label graphLabel, arraySizeLabel;
+    @FXML public Label graphLabel, timerLabel;
     @FXML public ComboBox<String> sortTypeComboBox;
-    @FXML public Slider arraySizeSlider;
     @FXML public Button startButton;
+    @FXML public BarChart chart;
+    @FXML public Spinner<Integer> sizeSpinner;
+
+    private final int MIN_SIZE = 1000;
+    private final int MAX_SIZE = 100000;
+    private final int STEP_SIZE = 100;
 
     private Sorter sorter;
+    private XYChart.Series series;
+    private List<XYChart.Data> nums;
+    private int arrSize;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         graphLabel.setText("");
+        series = new XYChart.Series();
+        chart.getData().add(series);
+        nums = new ArrayList<>();
+        arrSize = MIN_SIZE;
 
         // Combo Box Setup
         ObservableList<String> list = FXCollections.observableArrayList(SortType.getValuesAsString());
         sortTypeComboBox.setItems(list);
-        sortTypeComboBox.setOnAction(
-                e -> sorter = SortType.getSorterWithName(sortTypeComboBox.getSelectionModel().getSelectedItem()));
+        sortTypeComboBox.setOnAction(e -> {
+            sorter = SortType.getSorterWithName(sortTypeComboBox.getSelectionModel().getSelectedItem());
+            if (sizeSpinner.isDisabled())
+                sizeSpinner.setDisable(false);
+            updateText();
+        });
 
-
+        // Spinner Setup
+        sizeSpinner.setValueFactory(
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(MIN_SIZE, MAX_SIZE, MIN_SIZE, STEP_SIZE));
+        sizeSpinner.getValueFactory().setValue(MIN_SIZE);
+        sizeSpinner.valueProperty().addListener((obs, oldValue, newValue) -> {
+            arrSize = newValue;
+            updateText();
+        });
     }
+
+    private void updateText() {
+        graphLabel.setText(sorter.getDisplayableName() + " - " + arrSize);
+    }
+
+    /**
+     * Updates the chart based off the selected values of sort type and array size.
+     * A live visualisation of the array size helps the user gauge how big the array is
+     * that will be sorted.
+     */
+    private void updateChart() {
+
+        ObservableList<XYChart.Data> dataList = FXCollections.observableList(nums);
+        series.setData(dataList);
+    }
+
 
 }
