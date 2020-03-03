39e4793e64969ba30efcf9eb0846dbf6bc5da865
Eric Pedrido <epedrids@gmail.com>
2020-03-03 21:48:49 +1300

    Switched from chart to javafx rectangles to improve performance

--- src/main/java/com/pedrido/javafx/Controller.java
+++ src/main/java/com/pedrido/javafx/Controller.java
@@ -6,13 +6,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -20,29 +19,23 @@ public class Controller implements Initializable {
     @FXML public Label graphLabel, timerLabel;
     @FXML public ComboBox<String> sortTypeComboBox;
     @FXML public Button startButton;
-    @FXML public BarChart chart;
     @FXML public Spinner<Integer> sizeSpinner;
+    @FXML public Pane mainPane;
 
-    private final int MIN_SIZE = 10;
-    private final int MAX_SIZE = 1000;
-    private final int STEP_SIZE = 10;
+    private final int MIN_SIZE = 100;
+    private final int MAX_SIZE = 10000;
+    private final int STEP_SIZE = 50;
 
     private Sorter sorter;
-    private XYChart.Series series;
-    private List<XYChart.Data> nums;
+    private int[] nums;
     private int arrSize;
-
-    //TODO SWITCH FROM CHARTS TO MANUALLY PAINTING RECTANGLES SINCE CHARTS ARE SLOW AT LARGE VALUES
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         graphLabel.setText("");
-        series = new XYChart.Series();
-        chart.getData().add(series);
-        nums = new ArrayList<>();
         arrSize = MIN_SIZE;
 
-        increaseArraySize(MIN_SIZE);
+        setArrSize(MIN_SIZE);
 
         // Combo Box Setup
         ObservableList<String> list = FXCollections.observableArrayList(SortType.getValuesAsString());
@@ -59,49 +52,64 @@ public class Controller implements Initializable {
                 new SpinnerValueFactory.IntegerSpinnerValueFactory(MIN_SIZE, MAX_SIZE, MIN_SIZE, STEP_SIZE));
         sizeSpinner.getValueFactory().setValue(MIN_SIZE);
         sizeSpinner.valueProperty().addListener((obs, oldValue, newValue) -> {
-            if (newValue < arrSize) {
-                decreaseArraySize(newValue);
-            } else {
-                increaseArraySize(newValue);
-            }
-            arrSize = newValue;
-
+            setArrSize(newValue);
             updateText();
         });
-
-
     }
 
+    /**
+     * Updates the text about the bars which inform the user of sort algorithm and array size
+     */
     private void updateText() {
         graphLabel.setText(sorter.getDisplayableName() + " - " + arrSize);
     }
 
-    private void increaseArraySize(int newSize) {
-        for (int i = nums.size()+1; i <= newSize; i++) {
-            nums.add(new XYChart.Data<>(""+i, i));
+    /**
+     * Sets the size of the array being sorted in run time.
+     * User can select size and it will be portrayed live in {@link #updateBars()}
+     *
+     * @param newSize Size of new array
+     */
+    private void setArrSize(int newSize) {
+        nums = new int[newSize];
+        arrSize = newSize;
+
+        for (int i = 0; i < newSize; i++) {
+            nums[i] = i;
         }
 
-        updateChart();
-    }
-
-    private void decreaseArraySize(int newSize) {
-        for (int i = nums.size(); i > newSize; i--) {
-            nums.remove(nums.size()-1);
-        }
-
-        updateChart();
+        updateBars();
     }
 
     /**
-     * Updates the chart based off the selected values of sort type and array size.
+     * Updates the bars based off the selected array size.
      * A live visualisation of the array size helps the user gauge how big the array is
      * that will be sorted.
      */
-    private void updateChart() {
-        ObservableList<XYChart.Data> dataList = FXCollections.observableList(nums);
-        series.getData().clear();
-        series.getData().addAll(dataList);
+    private void updateBars() {
+        mainPane.getChildren().clear();
+
+        final double WIDTH = mainPane.getPrefWidth()/nums.length;
+        final double HEIGHT = mainPane.getPrefHeight(); // Depends on elements position in array
+        final double LENGTH = nums.length;
+
+        double x = 0; // Y value changes depending on height of the bar and so has no intial value
+
+        // Create a bar for each element in the array
+        for (int i = 0; i < LENGTH; i++) {
+            double barHeight = (i + 1)*HEIGHT/LENGTH;
+            Rectangle bar = new Rectangle();
+
+            bar.setX(x);
+            bar.setY(HEIGHT - barHeight);
+            bar.setWidth(WIDTH);
+            bar.setHeight(barHeight);
+            bar.setFill(Paint.valueOf("#5c5c5c"));
+
+            mainPane.getChildren().add(bar);
+
+            x += WIDTH;
+        }
+
     }
-
-
 }
